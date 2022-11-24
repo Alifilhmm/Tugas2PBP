54c2aa5e2135e52a47f1ff6762fb1237635fef36
alifilhmm <alifilhmm@gmail.com>
2022-11-24 11:13:08 +0700

    updated for tugas9

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.TextField()
-    title = models.TextField()
-    rating = models.FloatField()
-    release_date = models.TextField()
+    watched = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    release_date = models.DateField()
     review = models.TextField()
